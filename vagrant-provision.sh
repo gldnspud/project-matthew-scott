f941d397d148f4986ce457d0588f1b7d490a3893
Matthew Scott <matt@11craft.com>
2013-11-17 14:21:15 -0600

    Install Django 1.6 into environment - #1, #2

--- vagrant-provision.sh
+++ vagrant-provision.sh
@@ -25,14 +25,20 @@ chown vagrant.vagrant ${VENV_DIRS}
 
 if ! grep -q virtualenvwrapper /home/vagrant/.bashrc; then
   cat > /home/vagrant/.bash_profile <<EOF
-    . /usr/local/bin/virtualenvwrapper.sh
-    export WORKON_HOME=$HOME/.virtualenvs
+. /usr/local/bin/virtualenvwrapper.sh
+export WORKON_HOME=$HOME/.virtualenvs
 EOF
   cat >> /home/vagrant/.bashrc <<EOF
-    . /usr/local/bin/virtualenvwrapper.sh
-    export WORKON_HOME=$HOME/.virtualenvs
+. /usr/local/bin/virtualenvwrapper.sh
+export WORKON_HOME=$HOME/.virtualenvs
 EOF
   chown vagrant.vagrant /home/vagrant/.bash*
 fi
 
-sudo -u vagrant bash -login -c "mkvirtualenv t3; cd /vagrant; setvirtualenvproject"
+sudo -u vagrant bash -c "
+  . /usr/local/bin/virtualenvwrapper.sh;
+  mkvirtualenv t3;
+  cd /vagrant;
+  setvirtualenvproject;
+  pip install -r requirements.txt;
+"
