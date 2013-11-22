992bf66a7d2c66d6d4e4cde5459b77d5836db033
Matthew Scott <matt@11craft.com>
2013-11-22 11:20:45 -0600

    Specify location for collected static files

--- t3/settings.py
+++ t3/settings.py
@@ -115,6 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 
 
 # Template context
