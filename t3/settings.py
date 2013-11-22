764fb086f4b50965d35a262e786cee341ba5dacd
Matthew Scott <matt@11craft.com>
2013-11-22 11:40:35 -0600

    Fix: don't use django auth for permissions, just require authentication
    (Prior to this, only admins could write to the API.)

--- t3/settings.py
+++ t3/settings.py
@@ -157,9 +157,9 @@ REST_FRAMEWORK = {
     'DEFAULT_MODEL_SERIALIZER_CLASS':
         'rest_framework.serializers.HyperlinkedModelSerializer',
 
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
+    # Allow access to authenticated users.
+    # (user-based queryset prevents access to other users' items)
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissions',
+        'rest_framework.permissions.IsAuthenticated',
     ]
 }
